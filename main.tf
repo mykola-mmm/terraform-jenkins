a1b9d9512e682376a2f2e4fc15d2c68fdbaf6ffc
mykola-mmm <mykola.mazniuk@gmail.com>
2024-03-08 22:05:23 +0100

    added security-group module and some other minor changes

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ module "networking" {
   cidr_private_subnet = var.cidr_private_subnet
   availability_zone  = var.availability_zone
 }
+
+module "security_group" {
+  source = "./security-group"
+  ec2_sg_name = "SG for EC2 to enable SSH(22), HTTPS(443) and HTTP(80)"
+  ec2_jenkins_sg_name = "Allow port 8080 for jenkins"
+  vpc_id = module.networking.main_vpc_id
+}
